3860919653bb19acd257d25275450629196e9481
Marios Kotsiandris <marioskots@gmail.com>
2014-08-31 16:55:38 +0300

    Add maven dependencies for executable jar creation and apache options to run from command line.

--- src/main/java/com/thesis/Main.java
+++ src/main/java/com/thesis/Main.java
@@ -1,11 +1,49 @@
 package com.thesis;
 
 import com.thesis.models.ImageModel;
+import org.apache.commons.cli.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ImageModel imageModel = new ImageModel("rsz_30nm.png");
-		System.out.println(imageModel.calculatePorosityProcess().toString());
+		Options options = new Options();
+		Option fileNameProperty  = OptionBuilder.withArgName("property=value")
+				.hasArgs(2)
+				.withValueSeparator()
+				.withDescription( "enter the filepath" )
+				.create( "fileName" );
+		Option functionProperty  = OptionBuilder.withArgName("property=value")
+				.hasArgs(2)
+				.withValueSeparator()
+				.withDescription( "enter the function you want to run" )
+				.create( "function" );
+		options.addOption(fileNameProperty);
+		options.addOption(functionProperty);
+		// create the parser
+		CommandLineParser parser = new GnuParser();
+
+		try {
+			// parse the command line arguments
+			CommandLine line = parser.parse(options, args);
+			if(line.hasOption("fileName")) {
+				// initialise the member variable
+				String fileName = line.getOptionValue( "fileName" );
+				ImageModel imageModel = new ImageModel(fileName);
+					if (line.hasOption("function")){
+						String function = line.getOptionValue("function");
+							if (function.equals("p")){
+								System.out.println(imageModel.calculatePorosityProcess().toString());
+							} else if (function.equals("c")) {
+								imageModel.countParticlesProcess();
+							}
+						}
+					}
+			} catch (ParseException exp) {
+			exp.printStackTrace();
+			// oops, something went wrong
+//			HelpFormatter formatter = new HelpFormatter();
+//			formatter.printHelp("help", options );
+			System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+		}
 	}
 }
