e0037cfa91d879070b8de518bf3b10373850d575
Marios Kotsiandris <marioskots@gmail.com>
2014-09-13 21:30:37 +0300

    Adding file and make sure the image is 8 bit grayscale before process

--- src/main/java/com/thesis/helpers/ProcessHelper.java
+++ src/main/java/com/thesis/helpers/ProcessHelper.java
@@ -7,15 +7,22 @@ import ij.measure.Measurements;
 import ij.measure.ResultsTable;
 import ij.plugin.filter.Analyzer;
 import ij.plugin.filter.ParticleAnalyzer;
+import ij.process.ImageConverter;
+import ij.process.ImageProcessor;
 
 /**
  * Created by cerebro on 8/17/14.
  */
 public class ProcessHelper {
 	ImagePlus imagePlus;
+	ImageConverter imageConverter;
+	ImageProcessor imageProcessor;
 
 	public ProcessHelper(String filePath){
 		this.imagePlus = IJ.openImage(filePath);
+		this.imageConverter = new ImageConverter(this.imagePlus);
+		this.imageProcessor = this.imagePlus.getProcessor();
+		imageConverter.convertToGray8();
 	}
 
 	public Double getPorosity() throws NullPointerException{
@@ -51,15 +58,21 @@ public class ProcessHelper {
 	}
 
 	public void countParcicles(int width, int height){
-		this.imagePlus.getProcessor().setAutoThreshold("Default");
-		this.imagePlus.setRoi(0, 0, width, height - 100);
-		int measurments = Measurements.AREA+
-				Measurements.FERET+
-				Measurements.PERIMETER+
-				Measurements.CIRCULARITY;
-		ResultsTable rt = new ResultsTable();
-		ParticleAnalyzer particleAnalyzer = new ParticleAnalyzer(ParticleAnalyzer.SHOW_OUTLINES, measurments,rt, 10, 99999);
-		particleAnalyzer.analyze(this.imagePlus);
-		rt.show("My analyzer");
+		try {
+			this.imagePlus.getProcessor().setAutoThreshold("Default");
+	//		this.imagePlus.setRoi(0, 0, width, height - 100);
+			int measurments = Measurements.AREA+
+					Measurements.FERET+
+					Measurements.PERIMETER+
+					Measurements.CIRCULARITY;
+			ResultsTable rt = new ResultsTable();
+			ParticleAnalyzer particleAnalyzer = new ParticleAnalyzer(ParticleAnalyzer.SHOW_OUTLINES, measurments,rt, 10, 99999);
+			particleAnalyzer.analyze(this.imagePlus);
+			rt.show("My analyzer");
+		} catch (Exception e) {
+			System.out.println("Exception on countParticles");
+			e.printStackTrace();
+		}
+
 	}
 }
